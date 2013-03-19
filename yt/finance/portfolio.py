769dfaad1b4c3569445d648b0dacf16f5203c1a0
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-03-19 00:43:12 -0700

    adding portfolio

---
+++ yt/finance/portfolio.py
@@ -0,0 +1,14 @@
+"""
+encapsulates a portfolio
+"""
+
+
+class Portfolio(object):
+
+    def __init__(self):
+        pass
+
+    def return_rate(self):
+        """
+        Produce the rate of return of the portfolio
+        """
